bbeb99f9f1c2d11d94bd6c715eef11ca7e6dd6f4
bullbearan <bullbearan@gmail.com>
2022-07-06 12:37:45 +0100

    fix

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- This sql script lists all privileges of the MySQL users
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE states.name = 'California'
+WHERE states.name = 'California' AND cities.states_id = states.id
 ORDER BY cities.id
