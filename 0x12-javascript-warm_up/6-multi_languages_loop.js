9e21f0d1f19398713d16b695aa863d76367fd6e6
bullbearan <bullbearan@gmail.com>
2022-08-01 14:13:38 +0000

    test

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const a = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < a.length; i++) {
-  console.log(a[i]);
+const testing = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < testing.length; i++) {
+  console.log(testing[i]);
 }
